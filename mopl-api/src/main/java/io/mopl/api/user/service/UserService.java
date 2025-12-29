854314a7f0c02368280420d56264f0c25c123301
kjn4101 <wldls3866@gmail.com>
2025-12-29 17:21:36 +0900

    feat: 회원가입 기능 구현

--- mopl-api/src/main/java/io/mopl/api/user/service/UserService.java
+++ mopl-api/src/main/java/io/mopl/api/user/service/UserService.java
@@ -1,10 +1,16 @@
 package io.mopl.api.user.service;
 
+import io.mopl.api.user.domain.AuthProvider;
+import io.mopl.api.user.domain.User;
 import io.mopl.api.user.domain.UserRepository;
+import io.mopl.api.user.domain.UserRole;
+import io.mopl.api.user.dto.UserCreateRequest;
+import io.mopl.api.user.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -14,10 +20,33 @@ public class UserService {
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
 
-  //  /** 회원가입 */
-  //  @Transactional
-  //  public UserDto createUser(UserCreateRequest request) {
-  //    }
-  //  }
+  /** 회원가입 */
+  @Transactional
+  public UserDto createUser(UserCreateRequest request) {
+    if (userRepository.existsByEmail(request.getEmail())) {
+      throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+    }
 
+    User user =
+        User.builder()
+            .email(request.getEmail())
+            .name(request.getName())
+            .passwordHash(passwordEncoder.encode(request.getPassword()))
+            .role(UserRole.USER)
+            .authProvider(AuthProvider.LOCAL)
+            .locked(false)
+            .build();
+
+    User savedUser = userRepository.save(user);
+
+    return UserDto.builder()
+        .id(savedUser.getId())
+        .createdAt(savedUser.getCreatedAt())
+        .email(savedUser.getEmail())
+        .name(savedUser.getName())
+        .profileImageUrl(savedUser.getProfileImageUrl())
+        .role(savedUser.getRole())
+        .locked(savedUser.isLocked())
+        .build();
+  }
 }
