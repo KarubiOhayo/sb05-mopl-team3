da9d037043242febd18cbf2b1ea281cabd752955
kjn4101 <wldls3866@gmail.com>
2025-12-30 11:07:38 +0900

    feat: 사용자 정보 확인을 위한 UserSummary 메서드 추가

--- mopl-api/src/main/java/io/mopl/api/user/service/UserService.java
+++ mopl-api/src/main/java/io/mopl/api/user/service/UserService.java
@@ -1,5 +1,6 @@
 package io.mopl.api.user.service;
 
+import io.mopl.api.common.error.AuthErrorCode;
 import io.mopl.api.common.error.UserErrorCode;
 import io.mopl.api.user.domain.AuthProvider;
 import io.mopl.api.user.domain.User;
@@ -7,7 +8,9 @@ import io.mopl.api.user.domain.UserRepository;
 import io.mopl.api.user.domain.UserRole;
 import io.mopl.api.user.dto.UserCreateRequest;
 import io.mopl.api.user.dto.UserDto;
+import io.mopl.api.user.dto.UserSummary;
 import io.mopl.core.error.BusinessException;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -49,6 +52,21 @@ public class UserService {
         .profileImageUrl(savedUser.getProfileImageUrl())
         .role(savedUser.getRole())
         .locked(savedUser.isLocked())
+        .build();
+  }
+
+  /** 사용자 확인 */
+  @Transactional(readOnly = true)
+  public UserSummary getUserSummary(UUID userId) {
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new BusinessException(AuthErrorCode.USER_NOT_FOUND));
+
+    return UserSummary.builder()
+        .userId(user.getId())
+        .name(user.getName())
+        .profileImageUrl(user.getProfileImageUrl())
         .build();
   }
 }
