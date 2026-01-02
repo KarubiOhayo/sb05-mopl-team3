58927551b110b8c7045cffd96b0e36bafe5c8424
rlawnsry <color9117@naver.com>
2026-01-02 13:41:17 +0900

    fix: 컨텐츠 api가 spring MVC에서 절대 경로를 명시하도록 수정

--- mopl-api/src/main/java/io/mopl/api/content/controller/ContentController.java
+++ mopl-api/src/main/java/io/mopl/api/content/controller/ContentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/contents")
+@RequestMapping("/api/contents")
 public class ContentController {
 
   private final ContentService contentService;
