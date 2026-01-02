dc23ce5a547fbdbd5a807b6460a5427512175cf2
rlawnsry <color9117@naver.com>
2026-01-02 14:13:24 +0900

    fix: 포맷팅 이슈 해결

--- mopl-api/src/main/java/io/mopl/api/content/service/ContentService.java
+++ mopl-api/src/main/java/io/mopl/api/content/service/ContentService.java
@@ -5,10 +5,9 @@ import io.mopl.api.content.domain.Content;
 import io.mopl.api.content.domain.ContentRepository;
 import io.mopl.api.content.domain.ContentTagRepository;
 import io.mopl.api.content.dto.ContentDto;
+import io.mopl.core.error.BusinessException;
 import java.util.List;
 import java.util.UUID;
-
-import io.mopl.core.error.BusinessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,8 +25,10 @@ public class ContentService {
   public ContentDto findById(UUID contentId) {
     log.info("컨텐츠 단건 조회를 시작합니다. contentId: {}", contentId);
 
-    Content content = contentRepository.findById(contentId)
-        .orElseThrow(() -> new BusinessException(ContentErrorCode.CONTENT_NOT_FOUND));
+    Content content =
+        contentRepository
+            .findById(contentId)
+            .orElseThrow(() -> new BusinessException(ContentErrorCode.CONTENT_NOT_FOUND));
 
     List<String> tagNames = contentTagRepository.findTagNamesByContentId(contentId);
 
