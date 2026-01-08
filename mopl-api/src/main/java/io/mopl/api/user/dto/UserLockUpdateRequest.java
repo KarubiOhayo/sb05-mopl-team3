e8d6ad882415a74b4092dcf96f8b2a5d56974853
kjn4101 <wldls3866@gmail.com>
2026-01-08 16:30:08 +0900

    feat: User 도메인의 locked 필드에 @Setter 추가, UserLockUpdateRequest의 locked 필드에 @NotNull 어노테이션 추가

--- mopl-api/src/main/java/io/mopl/api/user/dto/UserLockUpdateRequest.java
+++ mopl-api/src/main/java/io/mopl/api/user/dto/UserLockUpdateRequest.java
@@ -1,5 +1,6 @@
 package io.mopl.api.user.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserLockUpdateRequest {
 
+  @NotNull(message = "잠금 상태는 필수입니다")
   private boolean locked;
 }
