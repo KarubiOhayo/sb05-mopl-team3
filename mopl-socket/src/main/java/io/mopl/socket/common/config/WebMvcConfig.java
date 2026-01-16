a93bff99714fa9d67e0ff7d0494633fb3abb210b
김규섭 <pens9053@gmail.com>
2026-01-16 10:32:31 +0900

    chore(socket): 보안 취약점이 될 수 있는 CORS 설정 변경

--- mopl-socket/src/main/java/io/mopl/socket/common/config/WebMvcConfig.java
+++ mopl-socket/src/main/java/io/mopl/socket/common/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package io.mopl.socket.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+  @Value("${app.cors.allowed-origins}")
+  private String[] allowedOrigins;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOriginPatterns("*")
+        .allowedOriginPatterns(allowedOrigins)
         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
